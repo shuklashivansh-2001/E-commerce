2ccf6748e0003d8eccff0caea0c8ac73047cbda0
shivansh shukla <hashmapmailer@gmail.com>
2024-10-06 20:13:14 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get('/health', (req, res) => {
+    res.send('Primary Backend is running and fully functional');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
